586f4802bcefe19ec66dc481026f85c354922de2
Doug Warren <doug78645@gmail.com>
2021-08-08 17:08:47 -0500

    Add version to Package.swift

--- RazeCore/Package.swift
+++ RazeCore/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RazeCore",
+    version: "0.1.0",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
